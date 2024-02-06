a3e43018e62ad6cf2c4b73f142e6ded31dc380c3
Gwenn Englebienne <dev@gwenn.dk>
2024-02-06 17:27:14 +0100

    Added readserial.py for debug

---
+++ python/readserial.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import serial
+import sys
+import time
+
+outfile = None
+if len(sys.argv) > 1:
+    outfile = open(sys.argv[1], "w+")
+portname = "/dev/ttyUSB0"
+if len(sys.argv) > 2:
+    portname = sys.argv[2]
+while True:
+    try:
+        with serial.Serial(portname, 921600, timeout=1) as ser:
+            while True:
+                l = ser.read().decode()
+                print(l, end='')
+                if outfile:
+                    outfile.write(l)
+    except serial.serialutil.SerialException:
+#        print("Could not open /dev/ttyUSB0")
+        time.sleep(1)
+    except KeyboardInterrupt:
+        exit(0)
+    except UnicodeDecodeError:
+        pass
+                    
+               
+          
+
+    
